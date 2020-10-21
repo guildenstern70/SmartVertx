c3a7d849d122a2d4423043b4f63b4cd113bef600
Alessio Saltarin <alessiosaltarin@gmail.com>
2020-10-21 10:00:06 +0200

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,67 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  kotlin ("jvm") version "1.4.10"
+  application
+  id("com.github.johnrengelman.shadow") version "5.2.0"
+}
+
+group = "net.littlelite"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+  jcenter()
+}
+
+val kotlinVersion = "1.4.10"
+val vertxVersion = "3.9.4"
+val junitJupiterVersion = "5.6.0"
+
+val mainVerticleName = "net.littlelite.SmartVertx.MainVerticle"
+val watchForChange = "src/**/*"
+val doOnChange = "./gradlew classes"
+val launcherClassName = "io.vertx.core.Launcher"
+
+application {
+  mainClassName = launcherClassName
+}
+
+dependencies {
+  implementation("io.vertx:vertx-web:$vertxVersion")
+  implementation("io.vertx:vertx-lang-kotlin:$vertxVersion")
+  implementation("io.vertx:vertx-web-templ-freemarker:$vertxVersion")
+  implementation(kotlin("stdlib-jdk8"))
+  testImplementation("io.vertx:vertx-junit5:$vertxVersion")
+  testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+}
+
+tasks.withType<KotlinCompile> {
+  kotlinOptions {
+    freeCompilerArgs = listOf("-Xjsr305=strict")
+    jvmTarget = "11"
+  }
+}
+
+tasks.withType<ShadowJar> {
+  archiveClassifier.set("fat")
+  manifest {
+    attributes(mapOf("Main-Verticle" to mainVerticleName))
+  }
+  mergeServiceFiles {
+    include("META-INF/services/io.vertx.core.spi.VerticleFactory")
+  }
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+  testLogging {
+    events = setOf(PASSED, SKIPPED, FAILED)
+  }
+}
+
+tasks.withType<JavaExec> {
+  args = listOf("run", mainVerticleName, "--redeploy=$watchForChange", "--launcher-class=$launcherClassName", "--on-redeploy=$doOnChange")
+}
